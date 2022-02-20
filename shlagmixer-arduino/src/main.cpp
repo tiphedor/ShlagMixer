99dacfbc7135a83816c46e28107baf3f9c0009ab
Martin STEFFEN <mail@tiph.io>
2022-02-20 15:08:40 +0100

    initial state note

--- shlagmixer-arduino/src/main.cpp
+++ shlagmixer-arduino/src/main.cpp
@@ -10,16 +10,18 @@ int SWITCHES_STATES[SWITCHES_COUNT];
 int FADERS_STATES[FADERS_COUNT];
 
 void setup() {
+  // Faders
   for (int i = 0; i < FADERS_COUNT; i++) {
     pinMode(FADERS_PINS[i], INPUT);
+    FADERS_STATES[i] = analogRead(FADERS_PINS[i]);
   }
 
 
+  // Switches
   for (int i = 0; i < SWITCHES_COUNT; i++) {
     pinMode(SWITCHES_PINS[i], INPUT_PULLUP);
+    SWITCHES_STATES[i] = digitalRead(SWITCHES_PINS[i]);
   }
-
-  MidiUSB.flush();
 }
 
 void loop() {
@@ -34,7 +36,6 @@ void loop() {
 
       // Send the note, we read a value withn 0-1023, MIDI uses 0-127, so remap the value
       controlChange(0, 32 + i, map(readState, 0, 1023, 0, 127));
-      MidiUSB.flush();
     }
   }
 
@@ -52,9 +53,37 @@ void loop() {
       } else {
         noteOff(0, 12 + i, 64);
       }
-      MidiUSB.flush();
     }
   }
 
+  midiEventPacket_t rx;
+  // Read incoming data - when we receive note 11 - send the current state.
+  do {
+    rx = MidiUSB.read();
+    if (rx.header != 0) {
+      byte command = rx.byte1;
+      byte note = rx.byte2;
+
+      // 144 = noteOn command - form the MIDI standard
+      // 11 = arbitrary and can be changed as long as its also changed in the ahk file.
+      // We don't care about velocity
+      if (command == 144 && note == 11) {
+        for (int i = 0; i < FADERS_COUNT; i++) {
+          controlChange(0, 32 + i, map(FADERS_STATES[i], 0, 1023, 0, 127));
+        }
+
+        for (int i = 0; i < SWITCHES_COUNT; i++) {
+          if (SWITCHES_STATES[i] == HIGH) {
+            noteOn(0, 12 + i, 64);
+          } else {
+            noteOff(0, 12 + i, 64);
+          }
+        }
+      }
+    }
+  } while (rx.header != 0);
+  
+
+  MidiUSB.flush();
   delay(25);
 }
